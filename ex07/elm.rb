64674ac253cdb5b9f71afb57beb34fce9e6c1314
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2024-10-25 22:01:12 +0800

    ok made more beautiful table, need to translate that to php

--- ex07/elm.rb
+++ ex07/elm.rb
@@ -10,25 +10,22 @@ def parse_element(line)
     attrs['name'] = name
     attrs
 end
-  
+
 def create_element_html(element)
     <<~HTML
-        <td style="border: 1px solid black; padding: 10px">
-            <h4>#{element['name']}</h4>
-            <ul>
-                <li>No #{element['number']}</li>
-                <li>#{element['small']}</li>
-                <li>#{element['molar']}</li>
-                <li>#{element['electron']} electron</li>
-            </ul>
+        <td class="element box">
+            <div class="number">#{element['number']}</div>
+            <div class="symbol">#{element['small']}</div>
+            <div class="name">#{element['name']}</div>
+            <div class="mass">#{element['molar']}</div>
         </td>
     HTML
 end
-  
+
 def create_empty_cell
-    '<td style="border: 1px solid black; padding: 10px"></td>'
+    '<td class="empty"></td>'
 end
-  
+
 def generate_periodic_table
     elements = File.readlines('periodic_table.txt').map { |line| parse_element(line.chomp) }
     
@@ -36,24 +33,78 @@ def generate_periodic_table
         <!DOCTYPE html>
         <html lang="en">
         <head>
+            <meta charset="UTF-8">
             <title>Periodic Table</title>
             <style>
-            table { border-collapse: collapse; }
-            td { vertical-align: top; width: 120px; }
-            ul { padding-left: 20px; margin: 5px 0; }
-            h4 { margin: 5px 0; }
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 20px;
+                    background-color: #f5f5f5;
+                }
+                
+                table {
+                    border-collapse: separate;
+                    border-spacing: 2px;
+                    margin: 0 auto;
+                }
+                
+                .element {
+                    width: 70px;
+                    height: 80px;
+                    padding: 4px;
+                    text-align: center;
+                    position: relative;
+                    transition: transform 0.2s;
+                    border-radius: 2px;
+                }
+                
+                .element:hover {
+                    transform: scale(2);
+                    z-index: 1;
+                    box-shadow: 0 0 10px rgba(0,0,0,0.3);
+                }
+                
+                .number {
+                    font-size: 10px;
+                    text-align: left;
+                }
+                
+                .symbol {
+                    font-size: 18px;
+                    font-weight: bold;
+                    margin: 4px 0;
+                }
+                
+                .name {
+                    font-size: 10px;
+                    margin-bottom: 2px;
+                }
+                
+                .mass {
+                    font-size: 9px;
+                    color: #666;
+                }
+                
+                .empty {
+                    width: 70px;
+                    height: 80px;
+                }
+
+                .box {
+                    background-color: #d6d6d6;
+                }
             </style>
         </head>
         <body>
             <table>
     HTML
-    
+
     current_row = []
     max_position = 17
     
     elements.each do |element|
         position = element['position'].to_i
-        
+
         while current_row.length < position
             current_row << create_empty_cell
         end
@@ -65,9 +116,9 @@ def generate_periodic_table
             current_row = []
         end
     end
-    
+
     html += "        <tr>#{current_row.join("\n")}</tr>\n" unless current_row.empty?
-    
+
     html += <<~HTML
             </table>
         </body>
@@ -82,7 +133,7 @@ generate_periodic_table
 =begin
 <?php
     $elements = [];
-    $lines = file('periodic_table.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $lines = file('ex06.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
         if (preg_match('/^(\w+)\s*=\s*position:(\d+),\s*number:(\d+),\s*small:\s*(\w+),\s*molar:([\d.]+),\s*electron:(.+)$/', $line, $matches)) {
             $elements[] = [
@@ -95,13 +146,12 @@ generate_periodic_table
             ];
         }
     }
-    $html = '<!-- python3 -m http.server 8080 -->
-    <!DOCTYPE html>
-    <html>
+    $html = '<!DOCTYPE html>
+    <html lang="en">
     <head>
         <title>Periodic Table</title>
         <style>
-            table { 
+            table {
                 border-collapse: collapse;
                 margin: 20px;
                 font-family: Arial, sans-serif;
@@ -139,34 +189,58 @@ generate_periodic_table
     $maxCols = 18;
     $table = array_fill(0, $maxRows, array_fill(0, $maxCols, null));
     foreach ($elements as $element) {
-        $row = floor($element['number'] / 18);
-        if ($element['number'] > 56 && $element['number'] < 72) continue;
-        if ($element['number'] > 88 && $element['number'] < 104) continue;
-        if ($element['number'] >= 72) $row = 5;
-        if ($element['number'] >= 104) $row = 6;
-        $table[$row][$element['position']] = $element;
+        $row = null;
+        $col = $element['position'];
+        $num = $element['number'];
+        
+        if ($num <= 2) {
+            $row = 0;
+        }
+        else if ($num <= 10) {
+            $row = 1;
+        }
+        else if ($num <= 18) {
+            $row = 2;
+        }
+        else if ($num <= 36) {
+            $row = 3;
+        }
+        else if ($num <= 54) {
+            $row = 4;
+        }
+        else if ($num <= 86) {
+            $row = 5;
+            if ($num > 57 && $num < 72) continue;
+        }
+        else {
+            $row = 6;
+            if ($num > 89 && $num < 104) continue;
+        }
+        if ($row !== null) {
+            $table[$row][$col] = $element;
+        }
     }
     for ($row = 0; $row < $maxRows; $row++) {
         $html .= '<tr>';
         for ($col = 0; $col < $maxCols; $col++) {
-            if ($table[$row][$col] === null) {
+            if (isset($table[$row][$col])) {
+                $element = $table[$row][$col];
+                $html .= '<td class="element">
+                    <h4>' . htmlspecialchars($element['name']) . '</h4>
+                    <ul>
+                        <li>No ' . $element['number'] . '</li>
+                        <li>' . htmlspecialchars($element['symbol']) . '</li>
+                        <li>' . $element['molar'] . '</li>
+                        <li>' . $element['electron'] . ' electron</li>
+                    </ul>
+                </td>';
+            } else {
                 $html .= '<td class="empty"></td>';
-                continue;
-            }       
-            $element = $table[$row][$col];
-            $html .= '<td class="element">
-                <h4>' . htmlspecialchars($element['name']) . '</h4>
-                <ul>
-                    <li>No ' . $element['number'] . '</li>
-                    <li>' . htmlspecialchars($element['symbol']) . '</li>
-                    <li>' . $element['molar'] . '</li>
-                    <li>' . $element['electron'] . ' electron</li>
-                </ul>
-            </td>';
+            }
         }
         $html .= '</tr>';
     }
     $html .= '</table></body></html>';
-    file_put_contents('periodic_table.html', $html);
+    file_put_contents('mendeleiev.html', $html);
 ?>
 =end
