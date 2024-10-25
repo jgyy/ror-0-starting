f7274fea1b9768abaf1a948f909ff0242fba0676
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2024-10-25 20:06:03 +0800

    made mistake with removal of h20

---
+++ ex02/H2o.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+def convert_to_hash
+    data = [['Caleb' , 24],
+            ['Calixte' , 84],
+            ['Calliste', 65],
+            ['Calvin' , 12],
+            ['Cameron' , 54],
+            ['Camil' , 32],
+            ['Camille' , 5],
+            ['Can' , 52],
+            ['Caner' , 56],
+            ['Cantin' , 4],
+            ['Carl' , 1],
+            ['Carlito' , 23],
+            ['Carlo' , 19],
+            ['Carlos' , 26],
+            ['Carter' , 54],
+            ['Casey' , 2]]
+
+    hash = {}
+    data.each do |name, age|
+      hash[age] = name
+    end
+
+    hash.each do |age, name|
+      puts "#{age} : #{name}"
+    end
+end
+  
+convert_to_hash
+
+=begin
+<?php
+    function array2hash($input_array) {
+        $result = array();
+        foreach ($input_array as $pair) {
+            if (count($pair) >= 2) {
+                $name = $pair[0];
+                $age = $pair[1];
+                $result[$age] = $name;
+            }
+        }
+        return $result;
+    }
+
+    function array2hash_sorted($array) {
+        $result = array();
+        foreach ($array as $item) {
+            if (count($item) >= 2) {
+                $name = $item[0];
+                $age = $item[1];
+                $result[$name] = $age;
+            }
+        }
+        krsort($result);
+        return $result;
+    }
+?>
+=end
