9e84dd9143d76f9950d50447204bd8f98bfdeeb5
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2024-10-27 15:33:29 +0800

    added python comment for ex00

--- ex00/var.rb
+++ ex00/var.rb
@@ -25,11 +25,36 @@ my_var
     $b = "10";
     $c = "ten";
     $d = 10.0;
-
     echo "My first variables:\n";
     echo "a contains : " . $a . " and has type : " . gettype($a) . "\n";
     echo "b contains : " . $b . " and has type : " . gettype($b) . "\n";
     echo "c contains : " . $c . " and has type : " . gettype($c) . "\n";
     echo "d contains : " . $d . " and has type : " . gettype($d) . "\n";
 ?>
+=end
+
+=being
+def my_var():
+    var1 = 42
+    var2 = "42"
+    var3 = "quarante-deux"
+    var4 = 42.0
+    var5 = True
+    var6 = [42]
+    var7 = {42: 42}
+    var8 = (42,)
+    var9 = set()
+    print(f"{var1} has a type {type(var1)}")
+    print(f"{var2} has a type {type(var2)}")
+    print(f"{var3} has a type {type(var3)}")
+    print(f"{var4} has a type {type(var4)}")
+    print(f"{var5} has a type {type(var5)}")
+    print(f"{var6} has a type {type(var6)}")
+    print(f"{var7} has a type {type(var7)}")
+    print(f"{var8} has a type {type(var8)}")
+    print(f"{var9} has a type {type(var9)}")
+
+if __name__ == '__main__':
+    my_var()
+
 =end
